7fb9e7c55b81eac817669c4464881f3d0c618b35
logonrm <logonrm@L2704MICRO28.fiap.com.br>
2017-08-07 23:01:20 -0300

    Exemplos cookie e filtro.

--- WebFiap/src/br/com/fiap/filter/PopulaUsuarioFilter.java
+++ WebFiap/src/br/com/fiap/filter/PopulaUsuarioFilter.java
@@ -8,23 +8,41 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 public class PopulaUsuarioFilter implements Filter {
 
 	private FilterConfig filterConfig = null;
-	
+
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
 
-		HttpServletRequest httpServletRequest = (HttpServletRequest)req;
+		HttpServletRequest httpServletRequest = (HttpServletRequest) req;
+
+		Cookie cookie = getUsuario(httpServletRequest);
 		
+
 		HttpSession session = httpServletRequest.getSession(true);
-		
+
 		chain.doFilter(req, resp);
-		
+
 	}
 
+	private Cookie getUsuario(HttpServletRequest req) {
+
+		Cookie[] cookies = req.getCookies();
+
+		if (cookies == null)
+			return null;
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("usuario.logado")) {
+				return cookie;
+			}
+		}
+
+		return null;
+	}
 }
