8a851e350a42e66ccd9fbf159b0e146582621ee5
logonrm <logonrm@L2704MICRO28.fiap.com.br>
2017-08-23 22:29:58 -0300

    commit

---
+++ fiap-aula4-java-web/src/br/com/fiap/bean/Aluno.java
@@ -0,0 +1,21 @@
+package br.com.fiap.bean;
+public class Aluno {
+
+private String nome;
+private String dataNasc;
+
+// getters and setters  
+public String getNome() {
+	return nome;
+}
+public void setNome(String nome) {
+	this.nome = nome;
+}
+public String getDataNasc() {
+	return dataNasc;
+}
+public void setDataNasc(String dataNasc) {	
+	this.dataNasc = dataNasc;
+}
+
+}
